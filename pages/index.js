48d9b3e0b8292c328ff1e40bd8c17fd87d90c72e
autaracharya <145379028+autaracharya@users.noreply.github.com>
2025-05-20 23:15:34 -0400

    Trigger rebuild with minor change

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
           animate={{ opacity: 1 }}
           transition={{ duration: 0.5 }}
         >
-          My Projects
+          My recent Projects
         </motion.h2>
         <motion.div
           className="grid grid-cols-1 md:grid-cols-2 gap-6 px-4"
